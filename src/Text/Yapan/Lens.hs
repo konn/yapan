5558b54ca4bc44ec44098db270702987779582b1
Hiromi Ishii <konn.jinro@gmail.com>
2015-03-05 23:11:19 +0900

    Added some basic lens & patterns

--- src/Text/Yapan/Lens.hs
+++ src/Text/Yapan/Lens.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE ConstraintKinds, FlexibleContexts, LiberalTypeSynonyms       #-}
 {-# LANGUAGE MultiParamTypeClasses, NoMonomorphismRestriction, RankNTypes #-}
-module Text.Yapan.Lens (_Str, _Emph, _Header, _Plain) where
+{-# OPTIONS_GHC -fwarn-unused-binds #-}
+module Text.Yapan.Lens (_Str, _Emph, _Header, _Plain, _Para, _List, _RawInline, _RawBlock) where
 import Control.Applicative (Applicative)
 import Control.Applicative (pure)
-import Data.Extensible     (Member)
 import Data.OpenUnion2
 import Data.Profunctor     (Choice)
 import Data.Profunctor     (right')
@@ -20,32 +20,50 @@ type Prism' s a = Prism s s a a
 prism' :: (b -> s) -> (s -> Maybe a) -> Prism s s a b
 prism' bs sma = prism bs (\s -> maybe (Left s) Right (sma s))
 
-_Str :: Member is Str => Prism' (Inline bs is) String
+_Str :: Element Str is => Prism' (Inline bs is) String
 _Str = prism' str $ \(Inline' a) ->
   case prj a of
     Just (Str' s) -> Just s
     Nothing -> Nothing
 
-_Emph :: Member is Emph => Prism' (Inline bs is) [Inline bs is]
+_Emph :: Element Emph is => Prism' (Inline bs is) [Inline bs is]
 _Emph = prism' emph $ \(Inline' a) ->
   case prj a of
     Just (Emph' s) -> Just s
     Nothing -> Nothing
 
-_Header :: Member bs Header => Prism' (Block bs is) (Int, [Inline bs is])
+_Header :: Element Header bs => Prism' (Block bs is) (Int, [Inline bs is])
 _Header = prism' (uncurry header) $ \(Block' a) ->
   case prj a of
     Just (Header' n s) -> Just (n, s)
     Nothing -> Nothing
 
-_Plain :: Member bs Plain => Prism' (Block bs is) [Inline bs is]
+_Plain :: Element Plain bs => Prism' (Block bs is) [Inline bs is]
 _Plain = prism' plain $ \(Block' a) ->
   case prj a of
     Just (Plain' s) -> Just s
     Nothing -> Nothing
 
-_Para :: Member bs Para => Prism' (Block bs is) [Inline bs is]
+_Para :: Element Para bs => Prism' (Block bs is) [Inline bs is]
 _Para = prism' para $ \(Block' a) ->
   case prj a of
     Just (Para' s) -> Just s
+    Nothing -> Nothing
+
+_RawInline :: Element RawInline is => Prism' (Inline bs is) (Format, String)
+_RawInline = prism' (uncurry rawInline) $ \(Inline' a) ->
+  case prj a of
+    Just (RawInline' fmt s) -> Just (fmt, s)
+    Nothing -> Nothing
+
+_RawBlock :: Element RawBlock bs => Prism' (Block bs is) (Format, String)
+_RawBlock = prism' (uncurry rawBlock) $ \(Block' a) ->
+  case prj a of
+    Just (RawBlock' fmt s) -> Just (fmt, s)
+    Nothing -> Nothing
+
+_List :: Element List bs => Prism' (Block bs is) (ListType, [[Block bs is]])
+_List = prism' (uncurry list) $ \(Block' a) ->
+  case prj a of
+    Just (List' fmt s) -> Just (fmt, s)
     Nothing -> Nothing
--- src/Text/Yapan/Patterns.hs
+++ src/Text/Yapan/Patterns.hs
@@ -6,16 +6,23 @@ module Text.Yapan.Patterns
         pattern Emph,
         pattern Header,
         pattern Plain,
-        pattern Para) where
+        pattern Para,
+        pattern RawBlock,
+        pattern List,
+        pattern RawInline
+       ) where
 import Text.Yapan.Base
 import Data.OpenUnion2
 
 pattern Block  a <- Block'  (prj -> Just a)
 pattern Inline a <- Inline' (prj -> Just a)
 
-pattern Str  s <- Inline (Str' s)
-pattern Emph e <- Inline (Emph' e)
+pattern Str  s         <- Inline (Str' s)
+pattern Emph e         <- Inline (Emph' e)
+pattern RawInline f s  <- Inline (RawInline' f s)
 
-pattern Header i hs <- Block (Header' i hs)
-pattern Plain  is   <- Block (Plain' is)
-pattern Para   is   <- Block (Para' is)
+pattern Header   i hs <- Block (Header' i hs)
+pattern Plain    is   <- Block (Plain' is)
+pattern Para     is   <- Block (Para' is)
+pattern RawBlock f s  <- Block (RawBlock' f s)
+pattern List tp bss   <- Block (List' tp bss)
